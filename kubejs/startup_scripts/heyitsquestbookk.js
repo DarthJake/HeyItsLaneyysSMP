ea387d3a406a341069e411bad8f31e5428c775bd
DarthJake <jacob@jellsworth.com>
2026-03-14 00:32:10 -0700

    Moved questbook texture to overwrite ftb questbook

--- kubejs/startup_scripts/heyitsquestbookk.js
+++ kubejs/startup_scripts/heyitsquestbookk.js
@@ -2,13 +2,13 @@
 
 // Visit the wiki for more info - https://kubejs.com/
 
-console.info('Creating HeyItsQuestBookk');
+// console.info('Creating HeyItsQuestBookk');
 
-StartupEvents.registry('item', event => {
-    event.create('heyitsquestbookk', 'basic')
-        .displayName("HeyItsQuestBookk")
-})
+// StartupEvents.registry('item', event => {
+//     event.create('heyitsquestbookk', 'basic')
+//         .displayName("HeyItsQuestBookk")
+// })
 
-StartupEvents.modifyCreativeTab('minecraft:misc', event => {
-    event.add('heyitsquestbookk')
-})
+// StartupEvents.modifyCreativeTab('minecraft:misc', event => {
+//     event.add('heyitsquestbookk')
+// })
